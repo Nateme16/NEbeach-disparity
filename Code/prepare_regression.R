c5e679ffed11f314eaaef1cf81a43ca6106f95b7
Nate Merrill <Nateme16@users.noreply.github.com>
2023-03-22 08:26:23 -0400

    Update prepare_regression.R

--- Code/prepare_regression.R
+++ Code/prepare_regression.R
@@ -24,7 +24,7 @@ readRenviron("~/.Renviron")
 statelist=c('AL','AK','AZ','AR','CA','CO','CT','DE','FL','GA','HI','ID','IL','IN','IA','KS','KY','LA','ME','MD','MA','MI','MN','MS','MO','MT','NE','NV','NH','NJ','NM','NY','NC','ND','OH','OK','OR','PA','RI','SC','SD','TN','TX','UT','VT','VA','WA','WV','WI','WY')
 #statelist=c('AL','AK')
 
-trips$GEOID=trips$Home
+trips$GEOID=trips$Home 
 
 
 allvarlist <- load_variables(2018, "acs5", cache = TRUE)
