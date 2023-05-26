607e0c2acbb7a44b39fd2bc162d4397bcaff7947
Eeburman <83651813+Eeburman@users.noreply.github.com>
2023-05-26 12:11:24 -0400

    Calculate geometric mean CFU

--- Code/prepare_bacteria.R
+++ Code/prepare_bacteria.R
@@ -12,13 +12,17 @@ wd= paste("C:/Users/",Sys.getenv("USERNAME"),"/Environmental Protection Agency (
 
 setwd(wd)
 
-bacteria=read.csv("data/beachAirsageJoined_03-09-2023-214244PM.csv")
+bacteria=read.csv("beachAirsageJoined_03-09-2023-214244PM.csv")
 
 bacteria=bacteria[,c("ActivityStartDate","ResultMeasureValue","Unique_ID" )]
 bacteria=bacteria[bacteria$ActivityStartDate!="",] #ask Erin about the blank activity start date
 
 bacteria$ResultMeasureValue[is.na(bacteria$ResultMeasureValue)]=0
 bacteria$cfu=as.numeric(bacteria$ResultMeasureValue)
+bacteria$cfuNoZeros = bacteria$cfu
+#replace zeros with ones so geometric mean is possible 
+bacteria$cfuNoZeros[bacteria$cfu == 0] <- 1
+
 bacteria=bacteria[!is.na(bacteria$cfu),]
 
 bacteria$poi=bacteria$Unique_ID
@@ -27,33 +31,37 @@ bacteria$year <- as.character(substr(bacteria$ActivityStartDate, 1, 4))
 bacteria$month <- as.character(substr(bacteria$ActivityStartDate, 6, 7))
 bacteria$day <- as.character(substr(bacteria$ActivityStartDate, 9, 10))
 
-bacteria=bacteria[,c("poi","cfu","year","month","day")]
+bacteria=bacteria[,c("poi","cfu", "cfuNoZeros", "year","month","day")]
 
 #collapse to year- 
 bacteria2= bacteria %>%
   group_by(poi,year) %>%
   summarise(cfu2=mean(cfu),
+            cfuGeomMean = exp(mean(log(cfuNoZeros))),
             n=n(),
             exceed100=sum(cfu > 100),
             exceed100perc=exceed100/n)
 
-bacteria2=bacteria2[,c("poi","cfu2","exceed100","exceed100perc","n","year")]
+bacteria2=bacteria2[,c("poi","cfu2", "cfuGeomMean", "exceed100","exceed100perc","n","year")]
 
-write.csv(bacteria2,"Data/bacteria_yearly.csv")
+write.csv(bacteria2,"bacteria_yearly.csv")
 
 
 #collapse to window of years- 
 bacteria_window= bacteria %>%
   group_by(poi) %>%
   summarise(cfu2=mean(cfu),
+            cfuGeomMean = exp(mean(log(cfuNoZeros))),
             n=n(),
             exceed100=sum(cfu > 100),
             exceed100perc=exceed100/n)
 
-bacteria_window=bacteria_window[,c("poi","cfu2","exceed100","exceed100perc","n")]
+bacteria_window=bacteria_window[,c("poi","cfu2","cfuGeomMean", "exceed100","exceed100perc","n")]
 
-write.csv(bacteria_window,"Data/bacteria_window.csv")
-
+write.csv(bacteria_window,"bacteria_window.csv")
+ggplot(data=bacteria_window, aes(x=cfuGeomMean)) +
+  geom_histogram(fill="steelblue", color="black") +
+  ggtitle("Histogram of cfus")
 
 
 
