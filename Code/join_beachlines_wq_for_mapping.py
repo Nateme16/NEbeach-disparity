6002a8470af1eeecaaddabe0cd5d5959d05cf839
Eeburman <83651813+Eeburman@users.noreply.github.com>
2023-07-16 18:23:12 -0400

    Update join_beachlines_wq_for_mapping.py
    Adding in demographics to joined feature class for even more mapping options.

--- Code/join_beachlines_wq_for_mapping.py
+++ Code/join_beachlines_wq_for_mapping.py
@@ -5,7 +5,7 @@ Created on Tue Mar 28 14:06:22 2023
 @author: EBURMA01
 
 README: 
-Script to join beach lines to cleaned wq data, for mapping 
+Script to join beach lines to cleaned wq data and demographic data, for mapping beach sites 
 """
 
 
@@ -40,18 +40,23 @@ env.workspace = os.path.join(workspacePath, workspaceName)
 
 #%%---------------------------------------------------------------------------
 # bring in wq data. table has to be arcGIS table view or similar
-wqPath = r"C:\Users\EBURMA01\Environmental Protection Agency (EPA)\ACESD Social Science Team - General\Research Projects\Beach research STRAP4\New England beach cell data\NEbeach-disparity\data\bacteria_window.csv"
+wqPath = os.path.join(dataFolder, r"bacteria_window.csv")
 arcpy.management.MakeTableView(wqPath, 'wqTable')
-
+# bring in demographics data, also make into arcGIS table view
+demPath = os.path.join(dataFolder, r"beach_demographics.csv")
+arcpy.management.MakeTableView(demPath, 'demTable')
 
 # airsage fc must be layer
 arcpy.MakeFeatureLayer_management('airSage', 'airSageLayer')
-                               
+
+# join everything together 
 arcpy.management.AddJoin('airSageLayer', 'Unique_ID',
                          'wqTable', 'poi',
                          )
-# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! join in visitation info too 
-#"beach_demographics.csv" maybe make this into a table view earlier?
+arcpy.management.AddJoin('airSageLayer', 'Unique_ID',
+                         'demTable', 'Poi',
+                         )
+
 
 # turn back into fc
 arcpy.env.overwriteOutput = True
