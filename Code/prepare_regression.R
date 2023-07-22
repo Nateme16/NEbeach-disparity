2a67aa8ee4664cfaf3c733c882420e73d57a9745
Eeburman <83651813+Eeburman@users.noreply.github.com>
2023-07-21 22:34:22 -0400

    Update prepare_regression.R
    reorganizing for new subsetted data approach and also so other humans can make sense of it

--- Code/prepare_regression.R
+++ Code/prepare_regression.R
@@ -1,28 +1,22 @@
 #prepare a regression for wq=f(demographics)
 
-library(here)
-library(pscl)
-library(boot)
-library(dplyr)
-library(tidyverse)
-library(car)
-library(lmtest)
-library(stargazer)
-library(AER)
-library(MASS)
-library(broom)
-library(randomForest)
-library(margins)
+'
+  ____                                 _   _             
+ |  _ \ _ __ ___ _ __   __ _ _ __ __ _| |_(_) ___  _ __  
+ | |_) | `__/ _ \ ,_ \ / _` | `__/ _` | __| |/ _ \| `_ \ 
+ |  __/| | |  __/ |_) | (_| | | | (_| | |_| | (_) | | | |
+ |_|   |_|  \___| .__/ \__,_|_|  \__,_|\__|_|\___/|_| |_|
+                |_|                                      
+Run all of this first 
+'
+# load packages
+pacman::p_load(here, pscl, boot, dplyr, tidyverse, car, lmtest, stargazer, AER, 
+               MASS, broom, margins)
 
-
-
-rm(list=ls()) #clear all
-
-####Create Airsage beaches POI summary info for all months together####
+rm(list=ls()) #clear environment 
 
 #build path to data files sensitive to usernames in OneDrive link
 wd= paste("C:/Users/",Sys.getenv("USERNAME"),"/Environmental Protection Agency (EPA)/ACESD Social Science Team - General/Research Projects/Beach research STRAP4/New England beach cell data/NEbeach-disparity/data", sep = "")
-
 setwd(wd)
 
 #bring in beach demographics
@@ -32,18 +26,25 @@ demog = rename(demog, poi = Poi)
 
 #connect wq to beaches window
 wq_window=read.csv("bacteria_window.csv")
-
 data=merge(wq_window,demog,by=c("poi")) 
 
 # remove unsupported null types
 data[is.na(data) | data=="Inf"] = NA
 
-#make median household income in thousands so coefficients 
+# make median household income in thousands so coefficients 
 # are easier to interpret 
 data$med_household_income_k = data$med_household_income / 1000
-
 #make pct exceedances out of 100 
 data$exceed100perc_x100 = data$exceed100perc * 100
+
+# make subsets removing outliers 
+# excluding 3 largest beaches 
+data_minus3 = subset(data, total < 9500000)
+# excluding 11 visitations above 5 million, a possible outlier cutoff
+data_minus11 = subset(data, total < 5000000)
+# excluding top 10% largest beaches 
+data_minus10pct = subset(data, total < 1556797.54)
+
 
 '
   _____ ____    _     
@@ -55,116 +56,16 @@ data$exceed100perc_x100 = data$exceed100perc * 100
   Exploratory Data Analysis                      
 
 '
-# important vars you might use 
-'
-poi    
-cfu2                          
-exceed100
-exceed100perc
-
-total
-
-white_pct                    
-black_pct   
-am_ind_ak_ntv_pct
-asian_pct                     
-ntv_hw_pac_isl_pct        
-some_other_race_pct          
-two_or_more_races_pct        
-two_races_including_pct     
-two_races_excluding_pct
-
-hispanic_or_latino_pct     
-not_h_white_pct                    
-not_h_black_pct                      
-not_h_ai_an_pct             
-not_h_asian_pct   
-
-med_household_income              
-med_home_value
-'
 
 #exploratory data analysis 
 head(data)
 summary(data)
 
-ggplot(data=data, aes(x=exceed100)) +
+# histogram of visitation, showing potential outlier cutoffs 
+ggplot(data=data, bins = 100, aes(x=total)) +
   geom_histogram(fill="steelblue", color="black") +
-  ggtitle("Histogram of exceedance numbers")
+  ggtitle("Histogram of total visitation") 
 
-ggplot(data=data, aes(x=exceed100perc_x100)) +
-  geom_histogram(fill="steelblue", color="black") +
-  ggtitle("Histogram of exceedance percents")
-
-#logged exceedences looks more normal 
-ggplot(data=data, aes(x=log(exceed100perc))) +
-  geom_histogram(fill="steelblue", color="black") +
-  ggtitle("Histogram of logged exceedance percents")
-
-ggplot(data=data, aes(x=cfuGeomMean)) +
-  geom_histogram(fill="steelblue", color="black") +
-  ggtitle("Histogram of cfus")
-
-#logged cfu also looks better
-ggplot(data=data, aes(x=log(cfuGeomMean))) +
-  geom_histogram(fill="steelblue", color="black") +
-  ggtitle("Histogram of logged cfus")
-
-ggplot(data=data, aes(x=white_pct)) +
-  geom_histogram(fill="steelblue", color="black") +
-  ggtitle("Histogram of pctwhite")
-
-ggplot(data=data, aes(x=n)) +
-  geom_histogram(fill="steelblue", color="black") +
-  ggtitle("Histogram of n")
-
-#scatter exceedance and pct white
-ggplot(data=data, aes(x=exceed100perc, y=white_pct)) + 
-  geom_point(alpha = 0.1)
-
-ggplot(data=data, aes(x=exceed100perc, y=cfu2)) + 
-  geom_point(alpha = 0.2)
-
-ggplot(data=data, aes(x=cfu2, y=exceed100perc)) + 
-  geom_point(alpha = 0.2)
-
-ggplot(data=data, aes(x=cfu2, y=cfuGeomMean)) + 
-  geom_point(alpha = 0.2)
-
-par(mfrow = c(1, 2))
-boxplot(data$cfu2)
-boxplot(data$exceed100perc)
-
-ggplot(data=data, aes(x=total, y=white_pct)) + 
-  geom_point(alpha = 0.1)
-
-ggplot(data=data, aes(x=black_pct, y=white_pct)) + 
-  geom_point(alpha = 0.1)
-
-ggplot(data=data, aes(x=exceed100perc, y=n)) + 
-  geom_point()
-
-ggplot(data=data, aes(x=exceed100perc, y=n)) + 
-  geom_point(alpha = 0.5)
-
-ggplot(data=data, aes(x=cfu2, y=n)) + 
-  geom_point(alpha = 0.1)
-
-ggplot(data=data, aes(x=exceed100perc, y=total)) + 
-  geom_point(alpha = 0.1)
-
-
-ggplot(data = data, aes(x = exceed100perc, y = n)) +
-  stat_density_2d(aes(fill = ..density..), geom = "tile", contour = FALSE) +
-  scale_fill_gradient(low = "white", high = "blue") +
-  labs(x = "exceed100perc", y = "n") +
-  theme_bw()
-
-# slice up data 
-
-# look for nulls 
-sapply(data, function(x) sum(is.na(x)))
-#no nulls
 
 # look at correlations 
 round(cor(data[c('exceed100perc', 'cfu2')]), 2)
@@ -199,61 +100,75 @@ round(cor(data[c('white_pct',
 
 '
 
-# weighted linear regression
-# weights by total visitation 
-# commonly used to reduce heteroskedacity or downweight imprecise/ low quality data points
-# sensitive to outliers
 
 
-reg_l = lm(exceed100perc~white_pct + hispanic_or_latino_pct + med_household_income,data=data)
-reg_lw = lm(exceed100perc_x100~white_pct + hispanic_or_latino_pct + med_household_income_k,weights = total,data=data)
-summary(reg_lw)
-vif(reg_lw)
-# sig white and hispanic/latino 
-# poor wq and pct hispanic/latino negatively correlated, interestingly 
-# vifs fine 
-# has weighting improved heteroskedacity issue? 
-plot(reg_lw$fitted.values, reg5w$residuals, xlab = "Fitted values", ylab = "Residuals")
-# these are also weird looking
-hist(reg_lw$residuals, breaks = 30) 
-#weird residuals
-# more diagnostics 
-par(mfrow = c(2, 2))
-plot(reg_lw)
-par(mfrow = c(1, 1))
-extractAIC(reg_lw)
-extractAIC(lm(exceed100perc~white_pct + hispanic_or_latino_pct + med_household_income + med_home_value,data=data, weights = total))
-extractAIC(lm(exceed100perc~white_pct + hispanic_or_latino_pct,data=data, weights = total))
-
-
-
-#log transforming exceedences
-reg_lwlog = lm(log(exceed100perc_x100+1)~white_pct + hispanic_or_latino_pct + med_household_income_k,data=data, weights = total)
-summary(reg_lwlog)
-hist(reg_lwlog$residuals, breaks = 30)
-par(mfrow = c(2, 2))
-plot(reg_lwlog)
-
-
-
-# cfu 
-regc = lm(log(cfu2)~white_pct + hispanic_or_latino_pct,data=data, weights = total)
-summary(regc)
-regcNoLog = lm(cfu2~white_pct + hispanic_or_latino_pct,data=data, weights = total)
-summary(regcNoLog)
-
-regcg = lm(log(cfuGeomMean)~white_pct + hispanic_or_latino_pct + med_household_income_k,data=data, weights = total)
-summary(regcg)
-
-regcg_log10 = lm(log10(cfuGeomMean)~white_pct + hispanic_or_latino_pct + med_household_income_k,data=data, weights = total)
-summary(regcg_log10)
+# Models of logged exceedances 
+    # Unweighted, entire dataset
+    r_exceed_noweights = lm(log(exceed100perc_x100+1)~white_pct + hispanic_or_latino_pct + med_household_income_k,data=data)
+    summary(r_exceed_noweights)
+    # Weighted, entire dataset
+    r_exceed_alldata =lm(log(exceed100perc_x100+1)~white_pct + hispanic_or_latino_pct + med_household_income_k,data=data, weights = total)
+    summary(r_exceed_alldata)
+    # Weighted, "minus 3" subset
+    r_exceed_minus3 = lm(log(exceed100perc_x100+1)~white_pct + hispanic_or_latino_pct + med_household_income_k,data=data_minus3, weights = total)
+    summary(r_exceed_minus3)
+    # Weighted, "minus 11" subset
+    r_exceed_minus11 = lm(log(exceed100perc_x100+1)~white_pct + hispanic_or_latino_pct + med_household_income_k,data=data_minus11, weights = total)
+    summary(r_exceed_minus11)
+    # Weighted, "minus 10 percent" subset 
+    r_exceed_minus10pct = lm(log(exceed100perc_x100+1)~white_pct + hispanic_or_latino_pct + med_household_income_k,data=data_minus10pct, weights = total)
+    summary(r_exceed_minus10pct)
+# Models of logged CFU
+    # Unweighted, entire dataset
+    r_cfu_noweights = lm(log(cfuGeomMean)~white_pct + hispanic_or_latino_pct + med_household_income_k,data=data)
+    summary(r_cfu_noweights)
+    # Weighted, entire dataset
+    r_cfu_alldata = lm(log(cfuGeomMean)~white_pct + hispanic_or_latino_pct + med_household_income_k,data=data, weights = total)
+    summary(r_cfu_alldata)
+    # Weighted, "minus 3" subset
+    r_cfu_minus3 = lm(log(cfuGeomMean)~white_pct + hispanic_or_latino_pct + med_household_income_k,data=data_minus3, weights = total)
+    summary(r_cfu_minus3)
+    # Weighted, "minus 11" subset
+    r_cfu_minus11 = lm(log(cfuGeomMean)~white_pct + hispanic_or_latino_pct + med_household_income_k,data=data_minus11, weights = total)
+    summary(r_cfu_minus11)
+    # Weighted, "minus 10 percent" subset 
+    r_cfu_minus10pct = lm(log(cfuGeomMean)~white_pct + hispanic_or_latino_pct + med_household_income_k,data=data_minus10pct, weights = total)
+    summary(r_cfu_minus10pct)
+# Model diagnostics and plots 
 
 
 
-par(mfrow = c(2, 2))
-plot(regcg)
 
 
+
+
+# predictions -- useful for interpreting log linear regression coefficients
+# for now, predicting for weighted regressions based on the "minus 11" subset, 
+# and for a scenario when var of interest increases/ decreases from median by 10 percent / $10k
+# but this section is flexible
+
+#define new example observation -- vary variable of interest and hold others constant 
+whiterObs = data.frame(white_pct=88, hispanic_or_latino_pct=5, med_household_income_k = 96)
+lesswhiteObs = data.frame(white_pct=78, hispanic_or_latino_pct=5, med_household_income_k = 96)
+
+lessHispanicObs = data.frame(white_pct=88, hispanic_or_latino_pct=5, med_household_income_k = 96)
+moreHispanicObs = data.frame(white_pct=88, hispanic_or_latino_pct=15, med_household_income_k = 96)
+
+richerObs = data.frame(white_pct=88, hispanic_or_latino_pct=5, med_household_income_k = 96)
+poorerObs = data.frame(white_pct=88, hispanic_or_latino_pct=5, med_household_income_k = 86)
+
+#use model to predict change in exceedance/cfu in scenario
+'example interpretion: A 10% increase (from 12% to 22%) in the chance that a 
+beach visit is by a non-white person is associated with a 3.7% increase in the 
+number of exceedances at that beach
+'
+lessWhitePred_Exceedance = exp(predict(r_exceed_minus11, whiterObs )) - exp(predict(r_exceed_minus11, lesswhiteObs))
+MoreHispanicPred_Exceedance = exp(predict(r_exceed_minus11, lessHispanicObs )) - exp(predict(r_exceed_minus11, moreHispanicObs))
+lessRichPred_Exceedance =   exp(predict(r_exceed_minus11, richerObs )) - exp(predict(r_exceed_minus11, poorerObs))
+  
+lessWhitePred_Exceedance = exp(predict(r_cfu_minus11, whiterObs )) - exp(predict(r_cfu_minus11, lesswhiteObs))
+MoreHispanicPred_Exceedance = exp(predict(r_cfu_minus11, lessHispanicObs )) - exp(predict(r_cfu_minus11, moreHispanicObs))
+lessRichPred_Exceedance =  exp(predict(r_cfu_minus11, richerObs )) - exp(predict(r_cfu_minus11, poorerObs))
 
 
 #stargazer output 
@@ -261,32 +176,15 @@ plot(regcg)
 reg_lw$AIC = AIC(reg5w)
 reg_lwlog$AIC = AIC(reg5wlog)
 regcg$AIC = AIC(reg5wlog)
-stargazer(reg_lw,reg_lwlog, regcg, header=FALSE,title="My Nice Regression Table", 
+
+# Table for exceedence models 
+stargazer(reg_lw,reg_lwlog, regcg, header=FALSE,title="Regression table", 
           type='text',
           keep.stat = c("n", "rsq", "adj.rsq", "ll", "f", "ser", "aic"),
           #keep.stat = c("all"),
           digits=3)
 
-# get interpretable marginal effects for log transformed models 
-margins(regcg)
-margins(reg_lwlog)
-
-
-# To calculate the Marginal Effect at the Mean (MEM) we have to obtain the mean values for each variable
-
-data_means_list = lapply(data,mean,na.rm=T)
-margins(regcg,at = mean(data$white_pct))
-margins(reg_lwlog,at = data_means_list)
-
-# predictions 
-#define new observation
-newdata1 = data.frame(white_pct=87, hispanic_or_latino_pct=5, med_household_income_k = 50)
-newdata2 = data.frame(white_pct=77, hispanic_or_latino_pct=5, med_household_income_k = 50)
-#use model to predict points value
-morewhite = exp(predict(reg_lwlog, newdata1))
-lesswhite = exp(predict(reg_lwlog, newdata2))
-morewhite - lesswhite
-
+# Table for CFU models 
 
 
 '
@@ -305,11 +203,6 @@ ggplot(data.frame(x=c(0, 1000)), aes(x)) +
   labs(x = "mean enterococci CFU", y = "illnesses per 1000 swimmers") + 
   stat_function(fun=function(x) 0.20 + (12.17*log10(x)))
 
-# function to put together our fitted cfu regression and rate function
-combineCfuRateEqs <- function(const, coeff1, coeff2, coeff3){
-  print(c("R = ", 0.20 + (12.17*const), " + ", coeff1*12.17, "*x1 + ", coeff2*12.17, "*x2 + ", coeff3*12.17 ))
-}
-combineCfuRateEqs(2.68, -0.02, -0.0059, -0.0019)
 
 # define function to quickly turn CFUs into rates 
 makeRate <- function(cfu){
@@ -327,57 +220,5 @@ rate_per1k_hispanic = makeRate(weighted.mean(data$cfuGeomMean, data$hispanic_or_
 
 
 
-
-
-
-
-## robustness tests 
-# high-visitation beaches may be misclassifying non-visitors as visitors 
-
-# excluding 1 largest beach 
-data_subset1 = subset(data, total < 14500000)
-# excluding 4 largest beaches 
-data_subset2 = subset(data, total < 9000000)
-# excluding 7 largest beaches
-data_subset3 = subset(data, total < 8000000)
-# excluding top 10% largest beaches 
-data_subset4 = subset(data, total < 1556797.54)
-# excluding top 25% largest beaches 
-data_subset5 = subset(data, total < 682699)
-
-
-
-# run regressions to see if still significant 
-
-# unlogged exceedances 
-summary(lm(exceed100perc_x100~white_pct + hispanic_or_latino_pct + med_household_income_k,weights = total,data=data))
-summary(lm(exceed100perc_x100~white_pct + hispanic_or_latino_pct + med_household_income_k,weights = total,data=data_subset1))
-summary(lm(exceed100perc_x100~white_pct + hispanic_or_latino_pct + med_household_income_k,weights = total,data=data_subset2))
-summary(lm(exceed100perc_x100~white_pct + hispanic_or_latino_pct + med_household_income_k,weights = total,data=data_subset3))
-summary(lm(exceed100perc_x100~white_pct + hispanic_or_latino_pct + med_household_income_k,weights = total,data=data_subset4))
-summary(lm(exceed100perc_x100~white_pct + hispanic_or_latino_pct + med_household_income_k,weights = total,data=data_subset5))
-
-# does not work but the intention is there
-for (subsets in 1: length(list(data_subset1, data_subset2, data_subset3, data_subset4, data_subset5))) {
-  print(summary(lm(log(exceed100perc_x100+1)~white_pct + hispanic_or_latino_pct + med_household_income_k,data=subsets, weights = total)))
-}
-# logged exceedences 
-summary(lm(log(exceed100perc_x100+1)~white_pct + hispanic_or_latino_pct + med_household_income_k,data=data, weights = total))
-summary(lm(log(exceed100perc_x100+1)~white_pct + hispanic_or_latino_pct + med_household_income_k,data=data_subset1, weights = total))
-summary(lm(log(exceed100perc_x100+1)~white_pct + hispanic_or_latino_pct + med_household_income_k,data=data_subset2, weights = total))
-summary(lm(log(exceed100perc_x100+1)~white_pct + hispanic_or_latino_pct + med_household_income_k,data=data_subset3, weights = total))
-summary(lm(log(exceed100perc_x100+1)~white_pct + hispanic_or_latino_pct + med_household_income_k,data=data_subset4, weights = total))
-summary(lm(log(exceed100perc_x100+1)~white_pct + hispanic_or_latino_pct + med_household_income_k,data=data_subset5, weights = total))
-# unweighted unsubsetted
-summary(lm(log(exceed100perc_x100+1)~white_pct + hispanic_or_latino_pct + med_household_income_k,data=data))
-
-
-#logged cfu 
-summary(lm(log(cfuGeomMean)~white_pct + hispanic_or_latino_pct + med_household_income_k,data=data, weights = total))
-summary(lm(log(cfuGeomMean)~white_pct + hispanic_or_latino_pct + med_household_income_k,data=data_subset1, weights = total))
-summary(lm(log(cfuGeomMean)~white_pct + hispanic_or_latino_pct + med_household_income_k,data=data_subset2, weights = total))
-summary(lm(log(cfuGeomMean)~white_pct + hispanic_or_latino_pct + med_household_income_k,data=data_subset3, weights = total))
-summary(lm(log(cfuGeomMean)~white_pct + hispanic_or_latino_pct + med_household_income_k,data=data_subset4, weights = total))
-summary(lm(log(cfuGeomMean)~white_pct + hispanic_or_latino_pct + med_household_income_k,data=data_subset5, weights = total))
 
 
